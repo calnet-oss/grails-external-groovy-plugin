946799e7f74ff3c2d7309b42d32cee7406ff8d7d
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-04-03 16:03:25 -0700

    reformat

--- grails-app/services/edu/berkeley/groovy/test/TestService.groovy
+++ grails-app/services/edu/berkeley/groovy/test/TestService.groovy
@@ -7,10 +7,10 @@ class TestService {
     // use by testGrailsApplicationScript.groovy
     static final String GRAILS_APPLICATION_SET = "grailsApplication is set"
     static final String GRAILS_APPLICATION_NOT_SET = "grailsApplication is not set"
-    
+
     ScriptRunner scriptRunner // injected
 
     String runScript(String scriptName) {
-      return scriptRunner.runScript(scriptName)
+        return scriptRunner.runScript(scriptName)
     }
 }
