646538a291f211d9c438a650aeab444b2ea70fe8
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-04-03 16:01:35 -0700

    add the TestService

---
+++ grails-app/services/edu/berkeley/groovy/test/TestService.groovy
@@ -0,0 +1,16 @@
+package edu.berkeley.groovy.test
+
+import edu.berkeley.groovy.ScriptRunner
+
+class TestService {
+
+    // use by testGrailsApplicationScript.groovy
+    static final String GRAILS_APPLICATION_SET = "grailsApplication is set"
+    static final String GRAILS_APPLICATION_NOT_SET = "grailsApplication is not set"
+    
+    ScriptRunner scriptRunner // injected
+
+    String runScript(String scriptName) {
+      return scriptRunner.runScript(scriptName)
+    }
+}
