9e00be85aaf7a6d786cc92801d9cc1227856695d
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-04-04 09:40:16 -0700

    Add support for checking for source file changes and recompiling if the source file has changed.  This is enabled using the recompileOnScriptChanges constructor parameter.

--- src/groovy/edu/berkeley/groovy/ScriptClassLoader.groovy
+++ src/groovy/edu/berkeley/groovy/ScriptClassLoader.groovy
@@ -1,5 +1,7 @@
 package edu.berkeley.groovy
 
+import org.codehaus.groovy.control.CompilationFailedException
+
 /**
  * A Groovy class loader that extends GroovyClassLoader to record in a
  * shared Statistics object when a ScriptClassLoader is loaded and
@@ -12,19 +14,40 @@ package edu.berkeley.groovy
 class ScriptClassLoader extends GroovyClassLoader {
 
     private Statistics stats
+    boolean recompileOnScriptChanges
 
-    ScriptClassLoader(Statistics stats) {
+    ScriptClassLoader() {
         super()
-        this.stats = stats
-        // increment the load count
-        stats.signalClassLoaderLoad()
+        verifyConstruction()
     }
 
-    ScriptClassLoader(ClassLoader parent, Statistics stats) {
+    ScriptClassLoader(Map map) {
+        super()
+        this.stats = map['stats']
+        this.recompileOnScriptChanges = map['recompileOnScriptChanges']
+        setShouldRecompile(recompileOnScriptChanges)
+        verifyConstruction()
+    }
+
+    ScriptClassLoader(Statistics stats, boolean recompileOnScriptChanges) {
+        super()
+        this.stats = stats
+        setShouldRecompile(recompileOnScriptChanges)
+        verifyConstruction()
+    }
+
+    ScriptClassLoader(ClassLoader parent, Statistics stats, boolean recompileOnScriptChanges) {
         super(parent)
         this.stats = stats
-        // increment the load count
-        stats.signalClassLoaderLoad()
+        setShouldRecompile(recompileOnScriptChanges)
+        verifyConstruction()
+    }
+
+    private void verifyConstruction() {
+        if (stats == null) {
+            throw new RuntimeException("Must pass a Statistics object into the constructor")
+        }
+        stats.signalClassLoaderLoad() // increment the load count
     }
 
     @Override
@@ -41,6 +64,50 @@ class ScriptClassLoader extends GroovyClassLoader {
             catch (Exception logException) {
                 e.printStackTrace()
             }
+        }
+    }
+
+    /*
+    @Override
+    public Class parseClass(GroovyCodeSource codeSource, boolean shouldCacheSource) throws CompilationFailedException {
+        //println("Running parseClass for ${codeSource.name}, isCachable=${codeSource.isCachable()}: shouldCacheSource=$shouldCacheSource")
+        super.parseClass(codeSource, shouldCacheSource)
+    }
+
+    def config = org.codehaus.groovy.control.CompilerConfiguration.DEFAULT
+    @Override
+    protected boolean isRecompilable(Class cls) {
+        boolean result = super.isRecompilable(cls)
+        if(cls != null) {
+          println("isRecompilable() for ${cls ? cls.name : 'null'} returning $result")
+          println("  isShouldRecompile() = " + isShouldRecompile())
+          println("  config.getRecompileGroovySource() = " + config.getRecompileGroovySource())
+          println("  (if true then not recompilable) a: " + (cls.getClassLoader() == this))
+          println("  (if true then not recompilable) b: " + (isShouldRecompile() == null && !config.getRecompileGroovySource()))
+          println("  (if true then not recompilable) c: " + (isShouldRecompile() != null && !isShouldRecompile()))
+          println("  (if true then not recompilable) d: " + (!GroovyObject.class.isAssignableFrom(cls)))
+          println("  (if true then not recompilable) e: " + (getTimeStamp(cls) == Long.MAX_VALUE))
+        }
+        return result
+    }
+
+    @Override
+    protected boolean isSourceNewer(URL source, Class cls) throws IOException {
+        boolean result = super.isSourceNewer(source, cls)
+        println("isSourceNewer() for source=$source, cls=${cls ? cls.name : 'null'} returning $result")
+        return result
+    }
+    */
+
+    @Override
+    protected Class loadClass(final String name, boolean resolve) throws ClassNotFoundException {
+        if (recompileOnScriptChanges) {
+            // We want preferClassOverScript to be false so that it checks
+            // the timestamp on the file for changes.
+            return super.loadClass(name, true, false, resolve)
+        } else {
+            // preferClassOverScript false
+            return super.loadClass(name, false, false, resolve)
         }
     }
 }
