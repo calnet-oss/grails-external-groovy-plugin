e46f4b132d173636e09c991f7dacd36cdd57fac5
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2015-04-06 14:30:15 -0700

    inject a Logger instance as 'log' property.  add a test for it.

---
+++ external-scripts/running/testLogInstance.groovy
@@ -0,0 +1,4 @@
+log.debug("testing the logger")
+// return back the injected log so the test can confirm its existence
+// results are returned as a String, so return class name rather than the object
+log.getClass().getName()
