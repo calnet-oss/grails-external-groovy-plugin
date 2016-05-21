21aa11b375c2c43fc4dce928af7d34a77ff97dc9
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2016-05-21 11:15:45 -0700

    change the group

--- ExternalGroovyGrailsPlugin.groovy
+++ ExternalGroovyGrailsPlugin.groovy
@@ -27,10 +27,10 @@
 import edu.berkeley.calnet.groovy.ScriptRunnerImpl
 
 class ExternalGroovyGrailsPlugin {
-    def group = "edu.berkeley.calnet.plugins"
+    def group = "edu.berkeley.calnet.grails.plugins"
 
     // the plugin version
-    def version = "1.0-SNAPSHOT"
+    def version = "1.0-SNAPSHOT" // !!! Change in build.gradle too
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
@@ -48,8 +48,7 @@ class ExternalGroovyGrailsPlugin {
     // initializes.
     def loadAfter = ["dataSource"]
 
-    // TODO Fill in these fields
-    def title = "External Groovy Plugin" // Headline display name of the plugin
+    def title = "External Groovy Plugin"
     def author = "Brian Koehmstedt"
     def authorEmail = "bkoehmstedt@berkeley.edu"
     def description = '''\
@@ -62,10 +61,9 @@ Execute external Groovy scripts from Grails.
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "BSD"
 
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def organization = [ name: "University of California, Berkeley", url: "http://www.berkeley.edu/" ]
 
     // Any additional developers beyond the author specified above.
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
@@ -77,7 +75,6 @@ Execute external Groovy scripts from Grails.
 //    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
 
     def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
     }
 
     def doWithSpring = {
@@ -108,25 +105,17 @@ Execute external Groovy scripts from Grails.
     }
 
     def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
     }
 
     def doWithApplicationContext = { ctx ->
-        // TODO Implement post initialization spring config (optional)
     }
 
     def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
     }
 
     def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
     }
 
     def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
     }
 }
